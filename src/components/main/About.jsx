5ca50860e9f3e21243f34251ec793cf9a7949d54
Vojtěch Bastl <bastlv.08@spst.eu>
2025-11-15 20:15:07 +0100

    projects + contact

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -2,11 +2,11 @@ function About() {
     return(
         <div className="bg-gray-800 p-4 text-white text">
             <div className="mx-3">
-                <h1 className="text-xl">About</h1>
-                <p>Jsem programátor, web developer a student Střední průmyslové školy Třebíč oboru Informační technologie.</p>
-                <p>Programování se věnuji více jak 4 roky a to od svých 13 let. Začal jsem s kurzy <a class="a" href="https://makeittoday.cz/">makeITtoday</a> v Pythonu a sám jsem se učil se stavebnicí Arduino.</p>
-                <p>V současnosti se věnuji tvorbě webových aplikací a v rámci školy tvorbě aplikací v C#.</p>
-                <p>Ve volném čase se mimo programování věnuji zvedání želez, hraní videoher a čtení knih.</p>
+                <h1 className="text-xl">About me</h1>
+                <p>I am a programmer, web developer, and a student of the High Industrial School in Třebíč, specializing in Information Technology.</p>
+                <p>I have been programming for over 4 years, starting at the age of 13. I began with courses from <a class="a" href="https://makeittoday.cz/">makeITtoday</a> in Python and taught myself using the Arduino kit.</p>
+                <p>Currently, I am focused on creating web applications and developing applications in C# as part of my studies.</p>
+                <p>In my free time, I enjoy weightlifting, playing video games, and reading.</p>
             </div>
         </div>
     )
